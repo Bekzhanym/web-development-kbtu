7a7ed8d3981d8288e28c4a45b342e7191958824f
Bekzhanym <maukenovbekzan@gmail.com>
2026-02-17 23:15:40 +0500

    lab 3 lab 4

--- lab4/task2/online-store/src/app/app.ts
+++ lab4/task2/online-store/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('lab4');
+  protected readonly title = signal('online-store');
 }
